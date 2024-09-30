dedbff03b7f2f5f9def30a34897e9faafd85672b
kdlibiran <kglibiran@up.edu.ph>
2024-09-30 19:25:05 +0800

    Change metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,12 +3,6 @@
 import { ThemeProvider } from "next-themes";
 import "./globals.css";
 import { useState, useEffect } from "react";
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Grade Calculator",
-  description: "Calculate your grade based on your score and total score.",
-};
 
 export default function RootLayout({
   children,
@@ -25,6 +19,10 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        <title>Grade Calculator</title>
+        <meta name="description" content="Calculate your grade based on your score and total score." />
+      </head>
       <body>
         <ThemeProvider attribute="class" defaultTheme="light">
           {children}
