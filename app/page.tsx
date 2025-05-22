0a30f0e5f7cdd7c7038d48485531c837af2edca6
kdlibiran <kglibiran@up.edu.ph>
2025-05-22 20:41:30 +0800

    add analytics

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import GradeCalculatorComponent from "@/components/grade-calculator";
-
+import { Analytics } from "@vercel/analytics/next";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
+      <Analytics />
       <GradeCalculatorComponent />
     </div>
   );
